b20f6c8d0a57441aef655136d32765159fffabaf
Samarth Deyagond <deyagondsamarth@gmail.com>
2019-09-22 14:10:54 +0530

    Added a landing url endpoint when the server is started

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
+app.use('/static', express.static('./static'));
 
 app.listen(3000, () => {
     console.log("It Works!");
 });
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './' });
+})
 
 app.get('/download', (req, res) => {
     var url = req.query.url;
