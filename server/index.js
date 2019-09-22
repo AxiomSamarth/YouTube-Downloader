872de1072797a192d1c1f39fda28b9dd379f6af6
Samarth Deyagond <deyagondsamarth@gmail.com>
2019-09-22 13:39:42 +0530

    Added landing HTML page and the node.js server to download the Youtube videos
    Signed-off-by: Samarth Deyagond <deyagondsamarth@gmail.com>

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("It Works!");
+});
+
+app.get('/download', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Video.mp4');
+    ytdl(url, {format: 'mp4'}).pipe(res);
+});
